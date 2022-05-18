5fb3c709b43b95680e68dd4e4d13200ad9f612aa
Juan Carlos <jhcarlos181@protonmail.com>
2022-05-17 23:45:22 -0500

    Se crea endpoint para realizar consulta de historial de moneda por medio de paginacion JDBC

--- src/main/java/com/co/indra/coinmarketcap/coins/services/CoinHistoryService.java
+++ src/main/java/com/co/indra/coinmarketcap/coins/services/CoinHistoryService.java
@@ -3,9 +3,9 @@ package com.co.indra.coinmarketcap.coins.services;
 import com.co.indra.coinmarketcap.coins.model.entities.CoinHistory;
 import com.co.indra.coinmarketcap.coins.repositories.CoinHistoryRepository;
 
-import javax.validation.constraints.Pattern;
-
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -14,12 +14,18 @@ public class CoinHistoryService {
 	@Autowired
 	CoinHistoryRepository coinHistoryRepository;
 
-	public void registerHistoryCoin(String symbol, Long idCoin, Double high, Double low,
-			Double closePrice, Double volume, Double marketCap) {
+	public void registerHistoryCoin(String symbol, Long idCoin, Double high, Double low, Double closePrice,
+			Double volume, Double marketCap) {
 
 		coinHistoryRepository
 				.createHistoryCoin(new CoinHistory(symbol, idCoin, high, low, closePrice, volume, marketCap));
 
+	}
+
+	public Page<CoinHistory> findAllCoinsHistoryPage(Pageable pageable) {
+
+		Page<CoinHistory> coinHistory = coinHistoryRepository.findAllCoinsHistoryPage(pageable);
+		return coinHistory;
 	}
 
 }
