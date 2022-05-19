581a34c81c1fc28c9cce4106d13e0c0340d80816
Juan Carlos <jhcarlos181@protonmail.com>
2022-05-19 11:09:10 -0500

    Se deja prueba CoinControllerTest como estaba anteriormente

--- src/test/java/com/co/indra/coinmarketcap/coins/controllers/CoinControllerTest.java
+++ src/test/java/com/co/indra/coinmarketcap/coins/controllers/CoinControllerTest.java
@@ -1,18 +1,19 @@
 package com.co.indra.coinmarketcap.coins.controllers;
 
+
 import com.co.indra.coinmarketcap.coins.config.Routes;
 import com.co.indra.coinmarketcap.coins.model.entities.Coin;
 import com.co.indra.coinmarketcap.coins.repositories.CoinRepository;
-//import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
-//import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.mock.web.MockHttpServletResponse;
-//import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
@@ -25,34 +26,38 @@ import java.util.List;
 @Transactional
 public class CoinControllerTest {
 
-   @Autowired
-   private MockMvc mockMvc;
+    @Autowired
+    private MockMvc mockMvc;
 
-   @Autowired
-   private CoinRepository coinRepository;
+    @Autowired
+    private CoinRepository coinRepository;
 
-   // @Autowired
-   // private ObjectMapper objectMapper;
+    @Autowired
+    private ObjectMapper objectMapper;
 
-   // @Autowired
-   // private JdbcTemplate jdbcTemplate;
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
 
-   @Test
-   public void createBasicCoinHappyPath() throws Exception {
-      MockHttpServletRequestBuilder request = MockMvcRequestBuilders
-            .post(Routes.COINS_PATH).content("{\r\n" + "    \"symbol\": \"TPU\",\r\n"
-                  + "    \"nameCoin\": \"PruebaCoin\",\r\n" + "    \"icon\": \"ICON\"\r\n" + "}")
-            .contentType(MediaType.APPLICATION_JSON);
 
-      MockHttpServletResponse response = mockMvc.perform(request).andReturn().getResponse();
-      Assertions.assertEquals(200, response.getStatus());
+    @Test
+    public void createBasicCoinHappyPath() throws Exception {
+        MockHttpServletRequestBuilder request = MockMvcRequestBuilders
+                .post(Routes.COINS_PATH)
+                .content("{\n" +
+                        "    \"symbol\": \"BTC\",\n" +
+                        "    \"nameCoin\": \"BIT COIN\",\n" +
+                        "    \"icon\": \"N/A\" \n" +
+                        "}").contentType(MediaType.APPLICATION_JSON);
 
-      List<Coin> coins = coinRepository.findBySymbol("TPU");
-      Assertions.assertEquals(1, coins.size());
+        MockHttpServletResponse response = mockMvc.perform(request).andReturn().getResponse();
+        Assertions.assertEquals(200, response.getStatus());
 
-      Coin CoinToAssert = coins.get(0);
+        List<Coin> coins = coinRepository.findBySymbol("BTC");
+        Assertions.assertEquals(1, coins.size());
 
-      Assertions.assertEquals("PruebaCoin", CoinToAssert.getNameCoin());
-      Assertions.assertEquals("ICON", CoinToAssert.getIcon());
-   }
+        Coin CoinToAssert = coins.get(0);
+
+        Assertions.assertEquals("BIT COIN", CoinToAssert.getNameCoin());
+        Assertions.assertEquals("N/A", CoinToAssert.getIcon());
+    }
 }
