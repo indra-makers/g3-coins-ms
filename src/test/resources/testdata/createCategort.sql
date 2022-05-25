405c658e87d937ec422e86b8e09a4613076f14fc
Juan Manuel Contreras Vega <64715084+JcontrerasV1997@users.noreply.github.com>
2022-05-24 22:27:11 -0500

    juanManuel/testPageable

---
+++ src/test/resources/testdata/createCategort.sql
@@ -0,0 +1,8 @@
+insert into tbl_categories(name_category,description) values('Solana','great');
+insert into tbl_categories(name_category,description) values('World','great');
+
+INSERT INTO tbl_coins(symbol, name_coin, icon) values('BIC', 'BIC COIN', 'N/A');
+INSERT INTO tbl_coins(symbol, name_coin, icon) values('ETH', 'ETHERUM', 'N/A');
+
+INSERT INTO tbl_coin_categories(id_category, symbol) values(1, 'BIC');
+INSERT INTO tbl_coin_categories(id_category, symbol) values(2, 'ETH');
