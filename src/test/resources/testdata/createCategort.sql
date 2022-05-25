96ae8fea1561089d319be2043ed5acc2c428425b
Juan Manuel Contreras Vega <64715084+JcontrerasV1997@users.noreply.github.com>
2022-05-25 09:03:39 -0500

    juanManuel/createView

--- src/test/resources/testdata/createCategort.sql
+++ src/test/resources/testdata/createCategort.sql
@@ -6,3 +6,10 @@ INSERT INTO tbl_coins(symbol, name_coin, icon) values('ETH', 'ETHERUM', 'N/A');
 
 INSERT INTO tbl_coin_categories(id_category, symbol) values(1, 'BIC');
 INSERT INTO tbl_coin_categories(id_category, symbol) values(2, 'ETH');
+
+create view category_coin
+			as
+SELECT  _intermediate.id_category,_coins.name_coin,_intermediate.symbol 
+	from tbl_categories _coinCategory
+	INNER JOIN tbl_coin_categories _intermediate on _intermediate.id_category =_coinCategory.id_category
+	INNER JOIN tbl_coins _coins on _coins.symbol= _intermediate.symbol
