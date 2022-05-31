367774a19494c086bd8395fa39c6df19c9850fa2
ZuluiZ <103589255+ZuluiZ@users.noreply.github.com>
2022-05-31 17:36:49 -0500

    PostConstruct

--- src/main/java/com/co/indra/coinmarketcap/coins/externalApi/repository/CoinRest.java
+++ src/main/java/com/co/indra/coinmarketcap/coins/externalApi/repository/CoinRest.java
@@ -8,13 +8,26 @@ import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
+import javax.annotation.PostConstruct;
 import java.util.HashMap;
 import java.util.Map;
+
 
 @Service
 public class CoinRest {
 
     private final RestTemplate template;
+    private Map<String, String> map = new HashMap<String, String>();
+
+    @PostConstruct
+    public void staticMap(){
+        String url= "https://api.coincap.io/v2/assets";
+        ListResponseBody listResponseBody = this.template.getForObject(url, ListResponseBody.class);
+        for(CoinModel coinModel: listResponseBody.getData())
+        {
+            map.put(coinModel.getSymbol(), coinModel.getId());
+        }
+    }
 
     public CoinRest(RestTemplateBuilder restTemplateBuilder){
         this.template=restTemplateBuilder.build();
@@ -25,18 +38,8 @@ public class CoinRest {
         return this.template.getForObject(url, ListResponseBody.class);
     }
 
-    public String getIdWithSymbol(String symbol){
-        String url= "https://api.coincap.io/v2/assets";
-        Map<String, String> map = new HashMap<String, String>();
-        ListResponseBody listResponseBody = template.getForObject(url, ListResponseBody.class);
-        for(CoinModel coin : listResponseBody.getData()){
-            map.put(coin.getSymbol(),coin.getId());
-        }
-        return map.get(symbol);
-    }
-
     public Coin getCoinBySymbol(String symbol){
-        String url = "https://api.coincap.io/v2/assets/" + getIdWithSymbol(symbol);
+        String url = "https://api.coincap.io/v2/assets/" + map.get(symbol);
         ResponseBody responseBody =  this.template.getForObject(url, ResponseBody.class);
         return new Coin(responseBody.getData().getSymbol(),responseBody.getData().getName(),responseBody.getData().getExplorer(),
                 responseBody.getData().getPriceUsd(),responseBody.getData().getVwap24Hr(),responseBody.getData().getChangePercent24Hr(),
